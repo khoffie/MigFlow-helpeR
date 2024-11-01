c6603b361f372ef51821f54f5b05b40a55fce821
Konstantin <konstantin.hoffie@ba-sachsen.de>
2024-11-01 10:24:23 +0100

    cleanup, gen_data does not need private pck modeleR anymore

--- helpeR/R/create_design_mat.R
+++ helpeR/R/create_design_mat.R
@@ -1,52 +1,61 @@
-##' Creates a design matrix from the INKAR data.table. Variables are
-##' selected and turned into wide format. Currently from a shapefile
-##' area is merged as additional column. Although this should actually
-##' be in the data already.
+##' Extracts INKAR variables
 ##'
-##' The columns are extracted using the inkar ID. These names are then
-##' turned into valid column names. Additionally all numerical zeros
-##' are set to 0.1. Then it is possible to log transform data that are
-##' numerically exactly zero as well. All columns are then log
-##' transformed.
-##'
-##' Also the variable X299, population density has missing values in
-##' the original INKAR data. Since population density is just
-##' population over area and this data is complete, I fill the missing
-##' values.
-##' 
-##' @title Create design matrix from INKAR data.table
 ##' @param ink data.table, INKAR
-##' @param ids character, ID'S of INKAR variables that are extracted
-##' @param rb character Raumbezug, here districts
-##' @param zb numeric, Zeitbezug, which years are used
-##' @param log logical. If TRUE, all columns are logarithmized.
+##' @param ids integer, IDs to extract
+##' @param rb character, Raumbezug: Spatial entities for which to
+##'     extract variables
+##' @param zb integer, Zeitbezug, years for which to extract
+##'     variabeles
+##' @param wide logical, if TRUE, data is returned in wide format. If
+##'     FALSE, long format is used instead
 ##' @return data.table
 ##' @import data.table
-##' @import inkaR
 ##' @export create_design_mat
-##' @author Konstantin Hoffie
-create_design_mat <- function(ink, ids, rb, zb, log = FALSE) {
-    ## I don't need chape anymore since I do not join area
-    . <- AGS <- X299 <- X4 <- X9 <- NULL
-    out <- inkaR::get_inkar_vars(ink,
-                                 ids,
-                                 rb = rb,
-                                 zb = zb, ## should be optional
-                                 name_col = "ID",
-                                 wide = TRUE)
-    ## out <- merge(out, shp[, .(AGS, area)],
-    ##              by = "AGS",
-    ##              all.x = TRUE)
-    make_valid_colnames(out)
-    if(299 %in% ids) {
-        out[is.na(X299), "X299" := X4 / X9]
+##' @author Konstantin
+create_design_mat <- function (ink, ids, rb, zb, wide = TRUE) {
+    out <- get_inkar_vars(ink, ids, rb = rb, zb = zb, wide = wide)
+    if(wide == TRUE) {
+        make_valid_colnames(out)
     }
-    ##     setnames(out, "area", "Xarea")
-    zeros_to_non_zeros(out, id_vars = c("AGS", "Zeitbezug"))
-    if (log == TRUE) {
-        columns_to_log(out, omit = c("AGS", "Zeitbezug"))
-    }
+    
+    ## if (299 %in% ids) {
+    ##     out[is.na(X299), `:=`("X299", X4/X9)]
+    ## }
+    ## zeros_to_non_zeros(out, id_vars = c("AGS", "Zeitbezug"))
     return(out)
+}
+
+##' Extracts variables from INKAR in a either long or wide format.
+##'
+##' @title Extract INKAR variables
+##' @param inkar data.table inkar data
+##' @param ids numeric, variable IDs
+##' @param rb character, "Raumbezug" like: "Bundesländer", "Kreise"
+##'     etc.
+##' @param zb numeric, Zeitbezug
+##' @param wide logical, if TRUE, data are returned in wide format, if
+##'     false, in long format.
+##' @return data.table
+##' @import data.table
+##' @author Konstantin
+get_inkar_vars <- function(inkar, ids, rb, zb, wide) {
+    ## lol this coding is so bad. would be much better to just read
+    ## the name_col
+#### Extrahiert Prediktoren aus dem INKAR Datensatz.
+
+    ## I should catch the warning:
+    ##    Aggregate function missing, defaulting to 'length'
+    ## convert to error and provide a more descriptive error message
+    Raumbezug <- . <- Wert <- NULL
+    region <- year <- ID <- NULL
+    ink <- inkar[Raumbezug %in% rb & year %in% zb & ID %in% ids, ]
+    ink <- ink[, .(region, rb = Raumbezug, id = ID, year, value = Wert)]
+    if (wide == TRUE) {
+        ink <- data.table::dcast(ink, region + rb + year ~ id,
+                                 value.var = "value")
+        make_valid_colnames(ink)
+    }
+    return(ink)
 }
 
 make_valid_colnames <- function(dt) {
@@ -55,56 +64,4 @@ make_valid_colnames <- function(dt) {
                      old = colnames(dt),
                      new = make.names(colnames(dt)))
     return(NULL)
-}
-
-zeros_to_non_zeros <- function(dt, id_vars = NULL) {
-    ## This looks very complicated. In the end I only want to
-    ## transform zeros in the predicors to 0.1 or something to be able
-    ## to use the logarithm. This might not need to be a function
-    ## because I can do it once when creating the design matrix
-    value <- variable <- N <- NULL
-    if (is.null(id_vars)) {
-        stop("Please specify id variables in dt.")
-    }
-    zeros <- data.table::melt(dt, id.vars = id_vars)
-    zeros <- zeros[value == 0, .N, keyby = variable]
-    if (nrow(zeros) == 0) {
-        mes <- "No zeros found in data. Data is kept as it is"
-        message(mes)
-    }
-    if (nrow(zeros) > 0) {
-    cols <- as.character(zeros[, unique(variable)])
-    mes <-  sprintf("Columns %s have in total %s zeros",
-                    paste0(cols, collapse = " "),
-                    zeros[, sum(N)])
-    message(mes)
-    dt[, (cols) := lapply(.SD, function(x) fifelse(x == 0, 0.1, x)),
-       .SDcols = cols]
-        message(sprintf(" %s zeros -> 0.1", zeros[, sum(N)]))
-    }
-    return(NULL)
-}
-
-columns_to_log <- function(dt, to_log = NULL, omit = NULL) {
-    ## used to transform columns in flows to log. But if I do
-    ## transformation earlier, when variables are extracted from
-    ## INKAR, I don't nee this function I think
-    if (is.null(to_log) && is.null(omit)) {
-        stop("Please specify either which columns to logarithmize or which to omit.")
-    }
-    if (!is.null(to_log) && is.null(omit)) {
-        cols <- to_log
-    }
-    if (!is.null(omit) && is.null(to_log)) {
-        cols <- base::setdiff(colnames(dt), omit)
-    }
-    bad <- dt[, lapply(.SD, function(x) any(x <= 0)), .SDcols = cols]
-    bad <- as.logical(bad)
-    if (sum(bad) > 0) {
-        mes <- sprintf("There are values <= 0 in columns %s",
-                       paste(cols[bad], collapse = " "))
-        stop(mes)
-    }
-    dt[, (cols) := lapply(.SD, log), .SDcols = cols]
-    return(dt)
 }
